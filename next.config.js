e65e2518c06a30c5b14e148c919ac3bd12f40bd0
sujan-0 <sujankhatiwoda0206@gmail.com>
2025-03-26 21:47:36 +0545

    Disable image optimization for static export

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    
+    images: {
+      unoptimized: true, // Disable image optimization for static export
+    },
+    trailingSlash: true, // Optional: Useful for static hosting
   };
   
   module.exports = nextConfig;
